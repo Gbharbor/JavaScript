d2fcfad24abd9709a0966bd7ea0b46a5c57a56a2
Drportox <guiportopt@outlook.com>
2023-12-11 20:29:43 +0000

    functions and arrays

---
+++ Modu 2/Script2.js
@@ -0,0 +1,84 @@
+//let colors = ["blue","green","red"];
+//console.log(colors);
+
+//let colors = ["blue", "red", "green"];
+//console.log(colors[2]);
+
+//let idades = [10, 20, 30];
+//console.log(idades);
+
+//let idades = [10, 20, 30];
+//console.log(idades[1]);
+
+//let lista = ["gui", 25, true];
+//console.log(lista);
+
+//let lista = ["gui", 25, true];
+//console.log(lista[0]);
+
+//let nomes = ["gui", "tete"];
+//let sobrenomes = ["porto", "barreto", nomes];
+//console.log(sobrenomes[2][1]);
+
+//let lista = ["gui", "tete", ["porto","barreto"]];
+//console.log(lista[2][0]);
+
+//let nomes = ["gui", "tete",["porto","barreto"]];
+//let sobrenomes = nomes[2];
+//console.log(sobrenomes[1]);
+
+
+//let ingredientes = [
+//    "ovo",
+//    "farinha",
+//    "agua",
+//];
+//console.log(`total: ${ingredientes.length}`);
+
+//let ingredientes = ["ovo","farinha"];
+//ingredientes[2] = "agua";
+//console.log(ingredientes);
+//console.log(`total: ${ingredientes.length}`);
+
+
+//let ingredientes = ["ovo","farinha","oleo"];
+//ingredientes.push("agua");
+//console.log(ingredientes);
+//console.log(`Total: ${ingredientes.length}`);
+
+//let nomes = [];
+//nomes.push("gui");
+//nomes.push("tete");
+//nomes.push("sandro");
+//console.log(nomes);
+//console.log(`Total: ${nomes.length}`);
+
+/*
+let nomes = [];
+nomes.push("gui");
+nomes.push("tete");
+nomes.pop();
+console.log(nomes);
+console.log(`total: ${nomes.length}`);
+
+let ingredientes = [];
+ingredientes.push("ovo");
+ingredientes.push("oleo");
+ingredientes.pop();
+console.log(ingredientes);
+console.log(`total: ${ingredientes.length}`);
+
+let nomes = [];
+nomes.push("gui");
+nomes.push("tete");
+nomes.shift();
+console.log(nomes);
+console.log(`total: ${nomes.length}`);
+
+let nomes = [];
+nomes.push("asd")
+nomes.push("ald")
+nomes.shift();
+console.log(nomes);
+console.log(`total: ${nomes.length}`);
+*/
