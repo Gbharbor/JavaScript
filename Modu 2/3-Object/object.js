f08c15f161d56b277a496ec198181d5014ce40ca
Drportox <guiportopt@outlook.com>
2024-01-14 21:05:38 +0000

    Clean code object

--- Modu 2/3-Object/object.js
+++ Modu 2/3-Object/object.js
@@ -1,20 +1,11 @@
 let character = {
-    name: "Bia",
-    lastname: "damelio",
-    fullname: function () {
-        return `${this.name} ${this.lastname}`;
-    },
+    name: "Bia",lastname: "damelio",
+    fullname: function () {return `${this.name} ${this.lastname}`;},
     age: [90,100],
-    allages: () => {
-        return character.age.length;
-    },
-    eyes: [
-        {size: "big", color: "black"},
-        {size: "small", color: "blue"}
-    ],
-    features: {
-        strength:[10,20,30],
-    }
+    allages: () => {return character.age.length;},
+    eyes: [{size: "big", color: "black"},
+           {size: "small", color: "blue"}],
+    features: {strength:[10,20,30],}
 }
 console.log(character.allages());
 console.log(character.fullname());
