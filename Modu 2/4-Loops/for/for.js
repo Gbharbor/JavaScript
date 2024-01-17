0a396ad6f2cf4e7218a345bee792e6ec0e24b368
Drportox <guiportopt@outlook.com>
2024-01-17 02:10:21 +0000

    loop for done

---
+++ Modu 2/4-Loops/for/for.js
@@ -0,0 +1,24 @@
+for (let n = 0; n < 10; n++) {
+    console.log("repeat" + n);}
+let items = ["item1","item2","item3"]; items.push("item4");
+for (let n=0; n < 10; n++) {
+    console.log(items[n]);} 
+for (let n=0; n<3; n++) {
+    console.log(items[n]);}
+for (let n = 0; n < items.length; n++) {
+    console.log(items[n]);}
+for (let i in items) {
+    console.log(items[i]);}
+let colors = [{name:"red", qt: "ten"},
+              {name:"blue", qt: "twenty"}];
+for (let types of colors) {
+    console.log(`Name: ${types.name} ${types.qt}`);}
+for (let i in colors) {
+    colors[i].name = colors[i].name.toUpperCase();
+    colors[i].qt = colors[i].qt.toUpperCase();
+    console.log(colors[i].qt);
+    console.log(colors[i].name);}
+for (let n=0; n < colors.length; n++) {
+    colors[n].qt = colors[n].qt.toLowerCase();
+    colors[n].name = colors[n].name.toLowerCase();
+    console.log(`${colors[n].qt} ${colors[n].name}`)}
