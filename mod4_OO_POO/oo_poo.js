405e0aa9060e506400d98a3afba35d664e9ec166
Drportox <guiportopt@outlook.com>
2024-01-28 00:00:28 +0000

    herenca

--- mod4_OO_POO/oo_poo.js
+++ mod4_OO_POO/oo_poo.js
@@ -69,3 +69,34 @@ let pers1 = new Person3("Joao","silva");
 let pers2 = new Person3("maria");
 pers1.age = 12;
 console.log(`${pers1.fullName} tem ${pers1.age} anos.`);
+
+
+class Person4 {
+    age = 0;
+    constructor(name) {
+        this.name = name;
+    }
+    sayHi() {
+        console.log(`${this.name} diz OI`)
+    }
+    sayWC() {
+        console.log(`Welcome ${this.name}`);
+    }
+}
+class Student extends Person4 {
+    constructor(name,id) { 
+        super(name);
+        this.id = id;
+    }
+    sayHi() {
+        console.log (`${this.name} e um estudante.`)
+    }
+    sayWC(){
+        super.sayWC();
+    }
+}
+let perso1 = new Student("Gui Porto", 1); 
+perso1.age = 25;
+perso1.sayHi();
+perso1.sayWC();
+console.log(`${perso1.name} tem #${perso1.age} anos e matricula ${perso1.id}`);
