6ba8505a659dfc42f1a424984e8e30e6f404dafb
Drportox <guiportopt@outlook.com>
2024-02-01 19:50:33 +0000

    review first steps

--- mod1_first_steps/intro.js
+++ mod1_first_steps/intro.js
@@ -1,147 +1,102 @@
-
-/*
-//Hello World
-!alert("hello world");
-console.log("hello world");
-*/
-/*
-//Variaveis
-let nome = "gui";
-let idade = 19;
-let lista = ["arroz", "feijao"];
-let nomeCompleto = {nome:"gui", idade:20};
-console.log(`${nome} + ${idade} + ${lista}`);
-console.log(nomeCompleto);
-*/
-/*
-//const 
-const idade = 20;
-console.log(idade);
-var nome = "gui";
+            //HELLO WORLD//
+!alert ("Hello World");
+console.log("hello world2.0")
+            //VARIAVEIS//
+let nombre = "gui";
+let age = 25; 
+let lista = ["gui","ana","Bia"];
+let object = {name: "Gui", age:24};
+let logado = true;
+            //CONST/VAR/LET//
+const idade = 23;
+var nome = "gio";
 console.log(nome);
-nome = "ana";
+nome = "bia";
 console.log(nome);
-*/
-/*
-//Operacoes em variaveis
-let x = 4;
-console.log(x);
-
-let nome = "gui";
-let sobrenome = "porto";
-let nomeCompleto = nome + ' ' + sobrenome;
-console.log(nomeCompleto);
-
-let n1 = 40;
+            //OPERACOES EM VARIAVEIS//
+let firstname = "gui";
+let lastname = "porto";
+let FullName = `${firstname} ${lastname}`;
+console.log(`usando operacoes em variaveis : ${FullName}`);
+let n1 = 20;
 let n2 = 20;
-let n3 = 30;
-
-let operacoes = (n1 + n2) - n3;
+let n3 = 10;
+let operacoes = `resultado: ${(n1 + n2) - n3}`
 console.log(operacoes);
-*/
-/*
-//template string
-let nome = "gui";
-let sobrenome = "porto";
-let nomeCompleto = `meu nome completo e : ${nome} ${sobrenome}`;
-
-console.log(nomeCompleto);
-*/
-//condicionais if/ else
-/*
-let idade = 10;
-if (idade>=18) {
-    console.log("vc e de maior");
+            //CONDICINAIS IF ELSE//
+let anos = 20;
+if (anos >=18) {
+    console.log(`Maior de Idade : ${anos}`);
 } else {
-    console.log("vc e de menor");
+    console.log(`Menor de Idade : ${anos}`);
 }
-*/
-/*
-//condicionais == e ===
-let number = 20 ;
-if (number === 20) {
-    console.log("vc pensou no numero certo")
+            //CONDICIONAIS == e ===//
+let number = 40;
+if (number === 40) {
+    console.log("Pensou no Number Certo");
 } else {
-    console.log("vc pensou no numero errado");
+    console.log("Pensou no Number Errado");
 }
-att de idade
-
-let idade = 20;
-if (idade === 20) {
-    idade = idade + 15;
-    console.log("vc att sua idade");
-    console.log(idade);
+let AttAge = 30;
+if (AttAge === 30) {
+    AttAge = AttAge + 2;
+    console.log(`vc att sua idade: ${AttAge}`);
 }
-*/
-
-//multicondicionais && e || 
-/*
-let idade = 110;
-if (idade >= 18 && idade < 60) {
-    console.log("vc e adulto");
+            //MULTICONDICIONAIS && e ||//
+let birth = 20;
+if (birth >= 18 && birth < 60) {
+    console.log("Vc e adulto!")
 }
-
-let idade = 14;
-if (idade >=18 || idade < 60) {
-    console.log("vc e um adulto");
+let birth1 = 12;
+if (birth1 >= 18 || birth1 < 60) {
+    console.log("vc e adulto ou talvez nao" + ' ' + birth1 + " anos");
 }
-*/
-/*
-//Condicionais duplas if else 
-let ok = true;
-if (ok){
-    console.log("entrou")
+            //CONDICIONAIS DUPLA IF ELSE//
+let ok = false;
+if (ok) {
+    console.log("entrou");
 } else {
     console.log("n entrou");
 }
-
-let idade = 19;
-let adulto = idade >= 18 && idade < 60; 
+let birth2 = 19;
+let adulto = idade >= 18 && idade < 60;
 if (adulto) {
     console.log("maior");
 } else {
     console.log("menor");
 }
-*/
-//condicionais duplas ELSE IF
-/*
-let idade = 10;
-if (idade < 18) {
-    console.log("minor");
-} else if (idade >=18 && idade < 60) {
-    console.log("of age");
-} else if (idade > 60) {
-    console.log("elderly")
+            //CONDICIONAIS DUPLAS ELSE IF//
+let ages = 12;
+if (ages < 18) {
+    console.log("Teen/kids");
+} else if (ages >=18 && ages < 60){
+    console.log("Adult");
+} else if (ages > 60) {
+    console.log("elderly");
 }
-*/
-
-//CONDICIONAIS TERNARIA 
-/*
+            //CONDICIONAIS TERNARIA//
 let isMember = false;
 let shipping = isMember ? 2 : 10;
-console.log(isMember ? "e membro" : "n e membro");
+console.log(isMember ? "is member" : "not member");
 console.log("frete: " + shipping);
 
-let age = 60;
-let isAdult = ((age>=18 && age < 60) ? "adulto" : "n adulto")
+let age1 = 14;
+let isAdult = ((age1>=18 && age1 > 60) ? "ADULTO" : "N ADULTO");
 console.log(isAdult);
-*/
-/*
+            //SWITCH//
 let profession = "fireman";
-console.log("profissao: " + profession);
-switch(profession) {
-    case "supervisor":
-        console.log("green shirt");
+console.log("Profissao: " + profession);
+switch (profession) {
+    case "supervisor": 
+        console.log("white tshirt");
     break;
-    case "fireman":
-        console.log("red shirt");
-    break
     case "police":
-        console.log("blue shirt");
+        console.log("blue tshirt");
     break;
-    
-    default: 
-        console.log("black shirt");
+    case "fireman" :
+        console.log("red tshirt");
+    break;
+    default :
+        console.log("black tshirt");
     break;
 }
-*/
